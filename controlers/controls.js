6f388f78b357f887911d34ce4e9b0f7eaa990254
Ritik <ritikrajbhagat1@gmail.com>
2023-07-02 00:43:47 +0530

    async handler added

--- controlers/controls.js
+++ controlers/controls.js
@@ -1,12 +1,13 @@
+const asyncHandler = require('express-async-handler') ;
 //it is a logic page
 
 // 1. GET /api/home
-const getHome =(req, res) => {
+const getHome = asyncHandler(async (req, res) => {
       res.status(200).json({messsage: "hello it is working"}) ;
-}
+})
 
 // 2. POST /api/home
-const postHome = (req, res) => {
+const postHome = asyncHandler(async (req, res) => {
       console.log(req.body);
       const {name, roll} = req.body ;
       if(!name || !roll){
@@ -14,6 +15,6 @@ const postHome = (req, res) => {
             throw new Error("All fields are mandatory !") ;
       }
       res.status(201).json({messsage :"new user created"}) ;
-}
+})
 
 module.exports = {getHome, postHome} ;
